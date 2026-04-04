6da7e3fcf498d0b1f16f92c8b8631f1a0aba5409
sugiura-hiromiti <pishadon57@gmail.com>
2026-04-04 11:18:44 +0900

    aa

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,12 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-	  '(auto-dark avy cape catppuccin-theme consult-eglot-embark corfu
-			diff-hl eat exec-path-from-shell haskell-ts-mode majutsu
-			marginalia md-ts-mode meow nerd-icons-corfu nix-ts-mode
-			nushell-ts-mode orderless org-modern pretty-hydra puni
-			rainbow-delimiters tree-sitter-langs vertico))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages '((majutsu :url "https://github.com/0WD0/majutsu"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

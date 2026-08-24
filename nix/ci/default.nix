d268a0521adefbfd343353f4d3c0251d5e03edb1
sugiura-hiromiti <pishadon57@gmail.com>
2026-08-24 13:23:12 +0900

    generate minimal github ci workflow

--- nix/ci/default.nix
+++ nix/ci/default.nix
@@ -24,10 +24,68 @@ let
   linuxHost = "aarch64-linux-a";
   darwinHost = "aarch64-darwin-a";
 
-  linuxNisosTarget = defaultTarget "nixos" linuxHost;
+  linuxNixosTarget = defaultTarget "nixos" linuxHost;
   linuxHomeTarget = defaultTarget "home" linuxHost;
 
-  darwinNisosTarget = defaultTarget "darwin" darwinHost;
+  darwinSystemTarget = defaultTarget "darwin" darwinHost;
   darwinHomeTarget = defaultTarget "home" darwinHost;
+
+  checkout = {
+    uses = "actions/checkout@v6";
+  };
+  installNix = {
+    uses = "cachix/install-nix-action@v31";
+  };
 in
-{ }
+{
+  workflows = {
+    ".github/workflows/ci.yml" = {
+      name = "CI";
+
+      on = {
+        push.branches = [ "main" ];
+        pull_request = { };
+        workflow_dispatch = { };
+      };
+
+      jobs = {
+        eval = {
+          runs-on = "ubuntu-24.04-arm";
+
+          steps = [
+            checkout
+            installNix
+            {
+              name = "Evaluate";
+              run = ''
+                nix flake check \
+                  --all-systems \
+                  --no-build \
+                  --no-write-lock-file
+              '';
+            }
+          ];
+        };
+
+        smoke-linux = {
+          runs-on = "ubuntu-24.04-arm";
+          needs = [ "eval" ];
+
+          steps = [
+            checkout
+            installNix
+            {
+              name = "Build representative Linux targets";
+              run = ''
+                nix build \
+                  ".#checks.aarch64-linux.build-nixos-${linuxNixosTarget}" \
+                  ".#checks.aarch64-linux.build-home-${linuxHomeTarget}" \
+                  --no-write-lock-file
+              '';
+            }
+          ];
+        };
+      };
+    };
+  };
+}

ef09d2e1aa4a25d8e8eaed2fc69b2ba5b77bc11b
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-10 19:24:46 +0900

    sync

--- nix/modules/nixos/features/storage/default.nix
+++ nix/modules/nixos/features/storage/default.nix
@@ -1,13 +1,25 @@
-{ lib, ... }: {
+{ lib, config, ... }:
+let
+  cfg = config.dotfiles.features.storage;
+in
+{
   options = {
     dotfiles = {
       features = {
         storage = {
+          partitionLabel = lib.mkOption {
+            type = lib.types.str;
+            default = "nixos";
+          };
+          device = lib.mkOption {
+            type = lib.types.str;
+            readOnly = true;
+            default = "/dev/disk/by-partlabel/${cfg.partitionLabel}";
+          };
           provisioning = {
             enable = lib.mkEnableOption "provisioning";
             disk = lib.mkOption { type = lib.types.str; };
           };
-          device = lib.mkOption { type = lib.types.str; };
           subvolumes = {
             root = lib.mkOption {
               type = lib.types.str;

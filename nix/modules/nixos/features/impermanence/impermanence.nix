eb9201508812df01e7b3377bc90361dd6a45435d
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-10 11:13:17 +0900

    sync

--- nix/modules/nixos/features/impermanence/impermanence.nix
+++ nix/modules/nixos/features/impermanence/impermanence.nix
@@ -11,6 +11,21 @@ in
             type = lib.types.bool;
             default = false;
           };
+          device = lib.mkOption { type = lib.types.str; };
+          subvolumes = {
+            root = lib.mkOption {
+              type = lib.types.str;
+              default = "@root";
+            };
+            persist = lib.mkOption {
+              type = lib.types.str;
+              default = "@persist";
+            };
+            nix = lib.mkOption {
+              type = lib.types.str;
+              default = "@nix";
+            };
+          };
         };
       };
     };

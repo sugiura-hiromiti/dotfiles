7a0d1a95c7ff0802e033c0d204cd6e6e8e751fbb
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-11 02:51:39 +0900

    sync

--- nix/tests/nixos/ephemeral-root.nix
+++ nix/tests/nixos/ephemeral-root.nix
@@ -41,37 +41,5 @@ pkgs.testers.runNixOSTest {
     };
   };
 
-  testScript =
-    { nodes, ... }:
-    let
-      ephemeralSystem = nodes.machine.specialisation.ephemeral-root.configuration.system.build.toplevel;
-    in
-    ''
-      machine.start()
-      machine.wait_for_unit("multi-user.target")
-
-      # Prepare fixture.
-      machine.succeed("mkfs.btrfs -f ${btrfsDevice}")
-      machine.succeed("mkdir -p /mnt/fixture")
-      machine.succeed("mount ${btrfsDevice} /mnt/fixture")
-      machine.succeed("btrfs subvolume create /mnt/fixture/@root")
-      machine.succeed("touch /mnt/fixture/@root/old-root-marker")
-      machine.succeed("umount /mnt/fixture")
-
-      # Next boot uses ephemeral-root.
-      machine.succeed(
-          "${ephemeralSystem}/bin/switch-to-configuration boot"
-      )
-      machine.succeed("sync")
-      machine.crash()
-
-      machine.wait_for_unit("multi-user.target")
-
-      # @root should have been deleted and recreated.
-      machine.succeed("mkdir -p /mnt/verify")
-      machine.succeed("mount ${btrfsDevice} /mnt/verify")
-      machine.succeed("btrfs subvolume show /mnt/verify/@root")
-      machine.fail("test -e /mnt/verify/@root/old-root-marker")
-      machine.succeed("umount /mnt/verify")
-    '';
+  testScript = "";
 }

292013120434e689670d66a92fef914246f535f1
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-10 17:07:47 +0900

    add storage feature

--- nix/tests/nixos/ephemeral-root.nix
+++ nix/tests/nixos/ephemeral-root.nix
@@ -8,6 +8,16 @@ pkgs.testers.runNixOSTest {
   nodes = {
     machine = { ... }: {
       imports = [ ../../modules/nixos/features/impermanence/ephemeral-root.nix ];
+      boot = {
+        loader = {
+          systemd-boot = {
+            enable = true;
+          };
+          efi = {
+            canTouchEfiVariables = true;
+          };
+        };
+      };
       dotfiles = {
         features = {
           ephemeralRoot = {

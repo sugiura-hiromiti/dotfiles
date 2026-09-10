8b8472f585cae5f47e0e0ff24531c25d095b9e94
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-10 11:46:48 +0900

    sync

--- nix/modules/nixos/features/impermanence/impermanence.nix
+++ nix/modules/nixos/features/impermanence/impermanence.nix
@@ -34,18 +34,18 @@ in
   config = lib.mkIf cfg.enable {
     fileSystems = {
       "/" = {
-        device = lib.mkDefault cfg.device;
+        device = cfg.device;
         fsType = "btrfs";
         options = [ "subvol=${cfg.subvolumes.root}" ];
       };
       "/persist" = {
-        device = lib.mkDefault cfg.device;
+        device = cfg.device;
         fsType = "btrfs";
         options = [ "subvol=${cfg.subvolumes.persist}" ];
         neededForBoot = true;
       };
       "/nix" = {
-        device = lib.mkDefault cfg.device;
+        device = cfg.device;
         fsType = "btrfs";
         options = [ "subvol=${cfg.subvolumes.nix}" ];
         neededForBoot = true;

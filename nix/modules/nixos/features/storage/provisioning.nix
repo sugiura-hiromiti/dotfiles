0cd0769416571ca4e6c562574c752d20f078911f
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-10 19:55:49 +0900

    sync

--- nix/modules/nixos/features/storage/provisioning.nix
+++ nix/modules/nixos/features/storage/provisioning.nix
@@ -1,1 +1,38 @@
-{ }
+{ lib, config, ... }:
+let
+  storage = config.dotfiles.features.storage;
+  cfg = storage.provisioning;
+in
+{
+  config = lib.mkIf cfg.enable {
+    disko = {
+      enableConfig = false;
+      devices = {
+        disk = {
+          main = {
+            type = "disk";
+            device = cfg.disk;
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = { };
+                system = {
+                  name = storage.partitionLabel;
+                  size = "100%";
+                  content = {
+                    type = "btrfs";
+                    subvolumes = {
+                      ${storage.subvolumes.root} = { };
+                      ${storage.subvolumes.nix} = { };
+                      ${storage.subvolumes.persist} = { };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

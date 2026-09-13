4a3b66ea56dbdfffcb0fd620824704b028a9de8c
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-14 00:42:46 +0900

    sync

--- nix/tests/nixos/storage-provisioning-vm.nix
+++ nix/tests/nixos/storage-provisioning-vm.nix
@@ -1,4 +1,32 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  disko,
+  ...
+}:
+let
+  diskoSystem = lib.nixosSystem {
+    system = pkgs.stdenv.hostPlatform.system;
+    modules = [
+      disko.nixosModules.disko
+      ../../modules/nixos/features/storage
+      {
+        dotfiles = {
+          features = {
+            storage = {
+              partitionLabel = "test-system";
+              provisioning = {
+                enable = true;
+                disk = "/dev/vdb";
+              };
+            };
+          };
+        };
+      }
+    ];
+  };
+  diskoScript = diskoSystem.config.system.build.diskoScript;
+in
 pkgs.testers.runNixOSTest {
   name = "dotfiles.storage-provisioning-vm";
   nodes = {
@@ -13,5 +41,7 @@ pkgs.testers.runNixOSTest {
     machine.wait_for_unit("multi-user.target")
 
     machine.succeed("test -b /dev/vdb")
+    machine.succeed("${diskoScript}")
+    machine.succeed("test -b /dev/disk/by-partlabel/test-system")
   '';
 }

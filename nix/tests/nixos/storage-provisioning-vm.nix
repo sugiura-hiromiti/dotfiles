071f28f83eb41d55070d690254d7c9a7233053a3
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-14 00:31:44 +0900

    sync

--- nix/tests/nixos/storage-provisioning-vm.nix
+++ nix/tests/nixos/storage-provisioning-vm.nix
@@ -1,3 +1,17 @@
-{ }: {
+{ pkgs, ... }:
+pkgs.testers.runNixOSTest {
+  name = "dotfiles.storage-provisioning-vm";
+  nodes = {
+    machine = {
+      virtualisation = {
+        emptyDiskImages = [ 1024 ];
+      };
+    };
+  };
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
 
+    machine.succeed("test -b /dev/vdb")
+  '';
 }

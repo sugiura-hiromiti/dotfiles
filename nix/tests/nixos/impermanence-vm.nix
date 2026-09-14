4426a6f119a93f7a30b8f45282a837bab32f1beb
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-14 09:39:06 +0900

    sync

--- nix/tests/nixos/impermanence-vm.nix
+++ nix/tests/nixos/impermanence-vm.nix
@@ -2,10 +2,15 @@
 pkgs.testers.runNixOSTest {
   name = "dotfiles.impermanence-vm";
   nodes = {
-    machine = { };
+    machine = {
+      virtualisation = {
+        emptyDiskImages = [ 1024 ];
+      };
+    };
   };
   testScript = ''
     machine.start()
     machine.wait_for_unit("multi-user.target")
+    machine.succeed("test -b /dev/vdb")
   '';
 }

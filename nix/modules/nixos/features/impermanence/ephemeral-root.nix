5a71d68afa8643ecf68130c4e094d025de44b1bc
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-16 14:24:52 +0900

    sync

--- nix/modules/nixos/features/impermanence/ephemeral-root.nix
+++ nix/modules/nixos/features/impermanence/ephemeral-root.nix
@@ -17,6 +17,10 @@ in
     dotfiles = {
       features = {
         ephemeralRoot = {
+          expectedFilesystemUuid = lib.mkOption {
+            type = lib.types.str;
+            description = "Expected UUID of the Btrfs filesystem before destructive reset";
+          };
           device = lib.mkOption {
             type = lib.types.str;
             description = "btrfs device containing the ephemeral @root subvolume";

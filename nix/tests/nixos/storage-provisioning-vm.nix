600d3d6efd9723869c82efe0633e9f4b2a0a0920
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-18 04:21:26 +0900

    sync

--- nix/tests/nixos/storage-provisioning-vm.nix
+++ nix/tests/nixos/storage-provisioning-vm.nix
@@ -9,8 +9,7 @@ let
   diskoSystem = lib.nixosSystem {
     system = pkgs.stdenv.hostPlatform.system;
     modules = [
-      disko.nixosModules.disko
-      ../../modules/nixos/features/storage
+      (import ../../modules/nixos/features/storage/provisioning.nix { inherit disko; })
       {
         dotfiles = {
           features = {
